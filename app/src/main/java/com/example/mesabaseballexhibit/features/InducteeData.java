c6f5862b6663f248a543183367332e0e48907126
Advaith Prakash <advaithprakash1304@gmail.com>
2025-09-30 02:07:27 -0700

    Filled in 2014 and 2016 Hall of Fame inductees, Players in years after 2016 do not have a bio, the "list" fxn requires a bio string, suggest to incorporate overloading or generate our own bios for the players.

--- app/src/main/java/com/example/mesabaseballexhibit/features/InducteeData.java
+++ app/src/main/java/com/example/mesabaseballexhibit/features/InducteeData.java
@@ -62,34 +62,71 @@ public class InducteeData {
             }
         if (year == 2016) {
             list.add(new com.example.mesabaseballexhibit.features.Inductee(R.drawable.dwight_patterson,
-                    "",
-                    "Mesa Rancher and Businessman",
-                    "One of the Cactus League's biggest civic boosters, he is credited with bringing the Chicago Cubs to Mesa in 1952 and then bringing them back to Mesa from Scottsdale in 1979. He was the first chairman of the storied Hohokams civic club, created to pursue a spring training baseball team in 1946 and was selected by Governor Rose Mofford to lead her task force to save spring training in the late 1980's."));
+                    "Dell Webb",
+                    "Co-Owner, New York Yankees 1947-1964",
+            "Del Webb is one of the most prominent names in Cactus League history. A lifelong fan of baseball,\n" +
+                    "Webb parlayed his success as the president of the Phoenix-based Del Webb Construction Co. into\n" +
+                    "\n" +
+                    "co-ownership of the Yankees. Del Webb is credited with one of the most interesting trades in base-\n" +
+                    "ball history. In 1951, Webb convinced New York Giants owner Horace Stoneham to swap spring\n" +
+                    "\n" +
+                    "training sites for a single year, sending the Giants to Florida and the Yankees to the original Phoenix\n" +
+                    "Municipal Stadium. The Yankees’ were the defending World Champions that year, with a lineup that\n" +
+                    "included Joe Dimaggio in his last season, Mickey Mantle and Yogi Berra, under the management of\n" +
+                    "manager Casey Stengel. Not only was Webb able to showcase the Yankees to other Arizona brass\n" +
+                    "as he was building Sun City—he gave fans a close-up look of a national market team years before\n" +
+                    "Major League Baseball’s westward expansion."));
             list.add(new com.example.mesabaseballexhibit.features.Inductee(R.drawable.dwight_patterson,
-                    "Dwight 'Pat' Patterson",
-                    "Mesa Rancher and Businessman",
-                    "One of the Cactus League's biggest civic boosters, he is credited with bringing the Chicago Cubs to Mesa in 1952 and then bringing them back to Mesa from Scottsdale in 1979. He was the first chairman of the storied Hohokams civic club, created to pursue a spring training baseball team in 1946 and was selected by Governor Rose Mofford to lead her task force to save spring training in the late 1980's."));
+                    "Gene Autry",
+                    "Owner, Los Angeles Angels 1961-1988",
+            "Autry was one of the most recognizable owners in baseball history, and a major contributor to the\n" +
+                    "success of spring training in Arizona. Though his Angels first trained in Palm Springs, Autry eventually\n" +
+                    "moved the team to Tempe Diablo Stadium in 1993. He would go on to lead later expansion efforts for\n" +
+                    "the Tempe Diablo complex. Known as the Singing Cowboy in the 1930s, Gene Autry moved on to\n" +
+                    "broadcasting before baseball came calling. In 1960, while looking to secure broadcasting rights to a\n" +
+                    "new California expansion team, he was instead persuaded to become the team’s owner—and the\n" +
+                    "Los Angeles Angels were born. After he died, the Angels retired the number 26 in his honor, reflecting\n" +
+                    "his position as the unofficial 26th member of the team’s 25-member roster. And in 1999, the Angels\n" +
+                    "renamed the stadium Tempe Diablo Gene Autry Field, and his famous ballad “Back in the Saddle”\n" +
+                    "continues to be played after each game."));
             list.add(new com.example.mesabaseballexhibit.features.Inductee(R.drawable.dwight_patterson,
-                    "Dwight 'Pat' Patterson",
-                    "Mesa Rancher and Businessman",
-                    "One of the Cactus League's biggest civic boosters, he is credited with bringing the Chicago Cubs to Mesa in 1952 and then bringing them back to Mesa from Scottsdale in 1979. He was the first chairman of the storied Hohokams civic club, created to pursue a spring training baseball team in 1946 and was selected by Governor Rose Mofford to lead her task force to save spring training in the late 1980's."));
+                    "Robert Brinton",
+                    "Mesa HohoKams President 2009-2010, Cactus League Board Member and President, 1989-2011",
+            "Robert Brinton had a close connection to the Cactus League and left an impression on everyone who\n" +
+                    "knew his efforts to support and expand it. Robert Brinton’s love of baseball started early. His father,\n" +
+                    "Dilworth Brinton, worked with Mesa rancher Dwight Patterson to help lure the Chicago Cubs to Mesa,\n" +
+                    "\n" +
+                    "and little Robert got his start selling Cubs spring training programs and watching players’ cars at Ren-\n" +
+                    "dezvous Park in the 1950s. A longtime member of the Mesa Hohokams, Brinton later served as presi-\n" +
+                    "dent of the Cactus League and the Mesa Visitors and Convention Bureau. He was instrumental in\n" +
+                    "\n" +
+                    "helping to preserve Major League Baseball’s presence in Arizona, tirelessly promoting the importance\n" +
+                    "\n" +
+                    "of the Cactus League as an economic engine and tourist draw to the Grand Canyon State. His politi-\n" +
+                    "cal efforts helped prevent the migration of teams to Florida, as did his leadership on the 2012 Keep\n" +
+                    "\n" +
+                    "the Cubs bond election in Mesa."));
             list.add(new com.example.mesabaseballexhibit.features.Inductee(R.drawable.dwight_patterson,
-                    "Dwight 'Pat' Patterson",
-                    "Mesa Rancher and Businessman",
-                    "One of the Cactus League's biggest civic boosters, he is credited with bringing the Chicago Cubs to Mesa in 1952 and then bringing them back to Mesa from Scottsdale in 1979. He was the first chairman of the storied Hohokams civic club, created to pursue a spring training baseball team in 1946 and was selected by Governor Rose Mofford to lead her task force to save spring training in the late 1980's."));
-            list.add(new com.example.mesabaseballexhibit.features.Inductee(R.drawable.dwight_patterson,
-                    "Dwight 'Pat' Patterson",
-                    "Mesa Rancher and Businessman",
-                    "One of the Cactus League's biggest civic boosters, he is credited with bringing the Chicago Cubs to Mesa in 1952 and then bringing them back to Mesa from Scottsdale in 1979. He was the first chairman of the storied Hohokams civic club, created to pursue a spring training baseball team in 1946 and was selected by Governor Rose Mofford to lead her task force to save spring training in the late 1980's."));
-            list.add(new com.example.mesabaseballexhibit.features.Inductee(R.drawable.dwight_patterson,
-                    "Dwight 'Pat' Patterson",
-                    "Mesa Rancher and Businessman",
-                    "One of the Cactus League's biggest civic boosters, he is credited with bringing the Chicago Cubs to Mesa in 1952 and then bringing them back to Mesa from Scottsdale in 1979. He was the first chairman of the storied Hohokams civic club, created to pursue a spring training baseball team in 1946 and was selected by Governor Rose Mofford to lead her task force to save spring training in the late 1980's."));
-
+                    "Jim Bruner",
+                    "Charro of the Year 1976-1977, Baseball Chairman 1980,\n" +
+                            "Hallstrom Lifer of the Year 2004-2005 and 2007-2008",
+                    "A mover and shaker in every sense of those words, Jim Bruner has certainly achieved legendary sta-\n" +
+                    "tus in Arizona’s baseball history. Pursuing his interest in bringing a Major League Baseball franchise\n" +
+                    "\n" +
+                    "to Arizona, Bruner worked his influence as a member of both the Scottsdale City Council and the\n" +
+                    "\n" +
+                    "Maricopa County Board of Supervisors to persuade sports mogul Jerry Colangelo to form an owner-\n" +
+                    "ship group. And, it was Bruner’s deciding vote that implemented the funding mechanism for a county-\n" +
+                    "owned stadium, home of the Arizona Diamondbacks. Bruner served for three and a half years as\n" +
+                    "\n" +
+                    "chairman of the Maricopa County Stadium District. He was a key player in developing a plan with\n" +
+                    "then-governor Rose Mofford to stabilize and expand the Arizona Cactus League in the late 1980s\n" +
+                    "when Florida was threatening to lure Cactus League teams to the Grapefruit League. Thanks to him,\n" +
+                    "fifteen MLB teams and their fans come to Arizona every spring to begin another exciting season."));
         }
         if (year == 2017) {
             list.add(new com.example.mesabaseballexhibit.features.Inductee(R.drawable.dwight_patterson,
-                    "Dwight 'Pat' Patterson",
+                    "Ernie Banks",
                     "Mesa Rancher and Businessman",
                     "One of the Cactus League's biggest civic boosters, he is credited with bringing the Chicago Cubs to Mesa in 1952 and then bringing them back to Mesa from Scottsdale in 1979. He was the first chairman of the storied Hohokams civic club, created to pursue a spring training baseball team in 1946 and was selected by Governor Rose Mofford to lead her task force to save spring training in the late 1980's."));
             list.add(new com.example.mesabaseballexhibit.features.Inductee(R.drawable.dwight_patterson,
