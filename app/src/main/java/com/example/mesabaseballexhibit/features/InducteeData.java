8050de4bce95dd55d08672af34be3ce070bed3ae
Advaith Prakash <advaithprakash1304@gmail.com>
2025-10-31 12:14:43 -0700

    Filled in 2014 and 2016 Hall of Fame inductees, Players in years after 2016 do not have a bio, the "list" fxn requires a bio string, suggest to incorporate overloading or generate our own bios for the players.

--- app/src/main/java/com/example/mesabaseballexhibit/features/InducteeData.java
+++ app/src/main/java/com/example/mesabaseballexhibit/features/InducteeData.java
@@ -15,114 +15,44 @@ public class InducteeData {
             list.add(new com.example.mesabaseballexhibit.features.Inductee(R.drawable.dwight_patterson,
                     "Horace Stoneham",
                     "Owner, New York/San Francisco Giants, 1936 - 1976",
-                    "Horace Stoneham is credited with helping to start the Cactus League when he moved his team to Phoenix\n" +
-                    "from Florida in 1947. Himself the owner of a winter home in Phoenix at the time, Stoneham also swapped\n" +
-                    "spring camps with Del Webb in 1951, when Mr. Webb proposed bringing his New York Yankees to the\n" +
-                    "\n" +
-                    "Cactus League for a season of baseball and selling homes. Stoneham's team spent more than two dec-\n" +
-                    "ades visiting the Buckhorn Baths and Mineral Springs in Mesa leaving a rich trail of spring training history\n" +
-                    "\n" +
-                    "and memories that continue to amaze fans today."));
+                    "Horace Stoneham is credited with helping to start the Cactus League when he moved his team to Phoenix from Florida in 1947. Himself the owner of a winter home in Phoenix at the time, Stoneham also swapped\n" +
+                    "spring camps with Del Webb in 1951, when Mr. Webb proposed bringing his New York Yankees to the Cactus League for a season of baseball and selling homes. Stoneham's team spent more than two dec-\n" +
+                    "ades visiting the Buckhorn Baths and Mineral Springs in Mesa leaving a rich trail of spring training historyand memories that continue to amaze fans today."));
             list.add(new com.example.mesabaseballexhibit.features.Inductee(R.drawable.dwight_patterson,
                     "Bill Veeck, Jr., 1946 - 1949",
                     "Owner, Cleveland Indians",
-                    "Bill Veeck, Jr. is credited with helping to start the Cactus League when he moved his team to Tucson\n" +
-                    "from Florida in 1947. Urged by Arizona State Senator Hiram Corbett of Tucson, Veeck wanted to move\n" +
-                    "his newly acquired team to be near his Southern Arizona guest ranch. He was able to accomplish his\n" +
-                    "goal when he found another owner willing to join him in the desert, N.Y. Giants owner Horace Stoneham."));
+                    "Bill Veeck, Jr. is credited with helping to start the Cactus League when he moved his team to Tucson from Florida in 1947. Urged by Arizona State Senator Hiram Corbett of Tucson, Veeck wanted to move his newly acquired team to be near his Southern Arizona guest ranch. He was able to accomplish his goal when he found another owner willing to join him in the desert, N.Y. Giants owner Horace Stoneham."));
             list.add(new com.example.mesabaseballexhibit.features.Inductee(R.drawable.dwight_patterson,
                     "Hon. Rose Mofford",
                     "Arizona Governor, 1988 - 1991",
-                    "Former Arizona Governor Rose Mofford was in the right place at the right time when it came to the task of\n" +
-                    "saving spring training baseball in Arizona. Florida interests were known to have contacted every team that\n" +
-                    "trained in Arizona at the time, but it was the threat of losing the Chicago Cubs and Cleveland Indians that\n" +
-                    "caused the most concern. In 1988, Governor Mofford, herself a former All-American softball player with\n" +
-                    "the Cantaloupe Queens, appointed a task force to study ways to keep teams training in Arizona. The task\n" +
-                    "force's work led to retention of the teams already here, the addition of new teams and an explosion in fan\n" +
-                    "attendance. Governor Mofford saved spring training and cemented the Cactus League as a fixture in the\n" +
-                    "state."));
+                    "Former Arizona Governor Rose Mofford was in the right place at the right time when it came to the task of saving spring training baseball in Arizona. Florida interests were known to have contacted every team that trained in Arizona at the time, but it was the threat of losing the Chicago Cubs and Cleveland Indians that caused the most concern. In 1988, Governor Mofford, herself a former All-American softball player with the Cantaloupe Queens, appointed a task force to study ways to keep teams training in Arizona. The task force's work led to retention of the teams already here, the addition of new teams and an explosion in fan attendance. Governor Mofford saved spring training and cemented the Cactus League as a fixture in the state."));
             list.add(new com.example.mesabaseballexhibit.features.Inductee(R.drawable.dwight_patterson,
                     "Ted and Alice Sliger",
                     "Owners of the Buckhorn Baths, Mesa, AZ, 1935 - 2010",
-                    "When their gas station burned down in 1935, the couple purchased ten acres of land at the northwest cor-\n" +
-                    "ner of Apache Trail and Recker Road then located miles east of Mesa. While digging a well, they discov-\n" +
-                    "ered a mineral spring that spurred the development of a series of small bathhouses, complete with a Ro-\n" +
-                    "man style bath and cooling room. The resort quickly became a favorite pre-Spring Training ritual for select\n" +
-                    "\n" +
-                    "members of the New York and San Francisco Giants and other baseball celebrities over the years."));
+                    "When their gas station burned down in 1935, the couple purchased ten acres of land at the northwest corner of Apache Trail and Recker Road then located miles east of Mesa. While digging a well, they discovered a mineral spring that spurred the development of a series of small bathhouses, complete with a Roman style bath and cooling room. The resort quickly became a favorite pre-Spring Training ritual for select members of the New York and San Francisco Giants and other baseball celebrities over the years."));
             list.add(new com.example.mesabaseballexhibit.features.Inductee(R.drawable.dwight_patterson,
                     "Hiram “Hi” Corbett",
                     "Arizona State Senator, Tucson, AZ, 1953 - 1964",
-                    "Hi Corbett was a Tucson native who is credited with helping to convince Bill Veeck to bring his Cleveland\n" +
-                            "Indians to Southern Arizona for spring training, giving rise to the Cactus League. Corbett was an amateur\n" +
-                            "golfer who organized Tucson's first professional golf tournament, was President of the Texas Baseball\n" +
-                            "\n" +
-                            "League, and served his community in the Arizona State Senate. The ballpark that played host to the Indi-\n" +
-                            "ans (and ultimately the expansion of the Colorado Rockies) was named after him in 1951."));
+                    "Hi Corbett was a Tucson native who is credited with helping to convince Bill Veeck to bring his Cleveland Indians to Southern Arizona for spring training, giving rise to the Cactus League. Corbett was an amateur golfer who organized Tucson's first professional golf tournament, was President of the Texas Baseball eague, and served his community in the Arizona State Senate. The ballpark that played host to the Indi ans (and ultimately the expansion of the Colorado Rockies) was named after him in 1951."));
             }
         if (year == 2016) {
             list.add(new com.example.mesabaseballexhibit.features.Inductee(R.drawable.dwight_patterson,
                     "Dell Webb",
                     "Co-Owner, New York Yankees 1947-1964",
-            "Del Webb is one of the most prominent names in Cactus League history. A lifelong fan of baseball,\n" +
-                    "Webb parlayed his success as the president of the Phoenix-based Del Webb Construction Co. into\n" +
-                    "\n" +
-                    "co-ownership of the Yankees. Del Webb is credited with one of the most interesting trades in base-\n" +
-                    "ball history. In 1951, Webb convinced New York Giants owner Horace Stoneham to swap spring\n" +
-                    "\n" +
-                    "training sites for a single year, sending the Giants to Florida and the Yankees to the original Phoenix\n" +
-                    "Municipal Stadium. The Yankees’ were the defending World Champions that year, with a lineup that\n" +
-                    "included Joe Dimaggio in his last season, Mickey Mantle and Yogi Berra, under the management of\n" +
-                    "manager Casey Stengel. Not only was Webb able to showcase the Yankees to other Arizona brass\n" +
-                    "as he was building Sun City—he gave fans a close-up look of a national market team years before\n" +
-                    "Major League Baseball’s westward expansion."));
+            "Del Webb is one of the most prominent names in Cactus League history. A lifelong fan of baseball, Webb parlayed his success as the president of the Phoenix-based Del Webb Construction Co. into co-ownership of the Yankees. Del Webb is credited with one of the most interesting trades in base ball history. In 1951, Webb convinced New York Giants owner Horace Stoneham to swap spring training sites for a single year, sending the Giants to Florida and the Yankees to the original Phoenix Municipal Stadium. The Yankees’ were the defending World Champions that year, with a lineup that included Joe Dimaggio in his last season, Mickey Mantle and Yogi Berra, under the management of manager Casey Stengel. Not only was Webb able to showcase the Yankees to other Arizona brass as he was building Sun City—he gave fans a close-up look of a national market team years before Major League Baseball’s westward expansion."));
             list.add(new com.example.mesabaseballexhibit.features.Inductee(R.drawable.dwight_patterson,
                     "Gene Autry",
                     "Owner, Los Angeles Angels 1961-1988",
-            "Autry was one of the most recognizable owners in baseball history, and a major contributor to the\n" +
-                    "success of spring training in Arizona. Though his Angels first trained in Palm Springs, Autry eventually\n" +
-                    "moved the team to Tempe Diablo Stadium in 1993. He would go on to lead later expansion efforts for\n" +
-                    "the Tempe Diablo complex. Known as the Singing Cowboy in the 1930s, Gene Autry moved on to\n" +
-                    "broadcasting before baseball came calling. In 1960, while looking to secure broadcasting rights to a\n" +
-                    "new California expansion team, he was instead persuaded to become the team’s owner—and the\n" +
-                    "Los Angeles Angels were born. After he died, the Angels retired the number 26 in his honor, reflecting\n" +
-                    "his position as the unofficial 26th member of the team’s 25-member roster. And in 1999, the Angels\n" +
-                    "renamed the stadium Tempe Diablo Gene Autry Field, and his famous ballad “Back in the Saddle”\n" +
-                    "continues to be played after each game."));
+            "Autry was one of the most recognizable owners in baseball history, and a major contributor to the success of spring training in Arizona. Though his Angels first trained in Palm Springs, Autry eventually moved the team to Tempe Diablo Stadium in 1993. He would go on to lead later expansion efforts for the Tempe Diablo complex. Known as the Singing Cowboy in the 1930s, Gene Autry moved on to broadcasting before baseball came calling. In 1960, while looking to secure broadcasting rights to a new California expansion team, he was instead persuaded to become the team’s owner—and the Los Angeles Angels were born. After he died, the Angels retired the number 26 in his honor, reflectingm his position as the unofficial 26th member of the team’s 25-member roster. And in 1999, the Angels renamed the stadium Tempe Diablo Gene Autry Field, and his famous ballad “Back in the Saddle continues to be played after each game."));
             list.add(new com.example.mesabaseballexhibit.features.Inductee(R.drawable.dwight_patterson,
                     "Robert Brinton",
                     "Mesa HohoKams President 2009-2010, Cactus League Board Member and President, 1989-2011",
-            "Robert Brinton had a close connection to the Cactus League and left an impression on everyone who\n" +
-                    "knew his efforts to support and expand it. Robert Brinton’s love of baseball started early. His father,\n" +
-                    "Dilworth Brinton, worked with Mesa rancher Dwight Patterson to help lure the Chicago Cubs to Mesa,\n" +
-                    "\n" +
-                    "and little Robert got his start selling Cubs spring training programs and watching players’ cars at Ren-\n" +
-                    "dezvous Park in the 1950s. A longtime member of the Mesa Hohokams, Brinton later served as presi-\n" +
-                    "dent of the Cactus League and the Mesa Visitors and Convention Bureau. He was instrumental in\n" +
-                    "\n" +
-                    "helping to preserve Major League Baseball’s presence in Arizona, tirelessly promoting the importance\n" +
-                    "\n" +
-                    "of the Cactus League as an economic engine and tourist draw to the Grand Canyon State. His politi-\n" +
-                    "cal efforts helped prevent the migration of teams to Florida, as did his leadership on the 2012 Keep\n" +
-                    "\n" +
-                    "the Cubs bond election in Mesa."));
+            "Robert Brinton had a close connection to the Cactus League and left an impression on everyone who knew his efforts to support and expand it. Robert Brinton’s love of baseball started early. His father, Dilworth Brinton, worked with Mesa rancher Dwight Patterson to help lure the Chicago Cubs to Mesa, helping to preserve Major League Baseball’s presence in Arizona, tirelessly promoting the importance cal efforts helped prevent the migration of teams to Florida, as did his leadership on the 2012 Keep_the Cubs bond election in Mesa."));
             list.add(new com.example.mesabaseballexhibit.features.Inductee(R.drawable.dwight_patterson,
                     "Jim Bruner",
                     "Charro of the Year 1976-1977, Baseball Chairman 1980,\n" +
                             "Hallstrom Lifer of the Year 2004-2005 and 2007-2008",
-                    "A mover and shaker in every sense of those words, Jim Bruner has certainly achieved legendary sta-\n" +
-                    "tus in Arizona’s baseball history. Pursuing his interest in bringing a Major League Baseball franchise\n" +
-                    "\n" +
-                    "to Arizona, Bruner worked his influence as a member of both the Scottsdale City Council and the\n" +
-                    "\n" +
-                    "Maricopa County Board of Supervisors to persuade sports mogul Jerry Colangelo to form an owner-\n" +
-                    "ship group. And, it was Bruner’s deciding vote that implemented the funding mechanism for a county-\n" +
-                    "owned stadium, home of the Arizona Diamondbacks. Bruner served for three and a half years as\n" +
-                    "\n" +
-                    "chairman of the Maricopa County Stadium District. He was a key player in developing a plan with\n" +
-                    "then-governor Rose Mofford to stabilize and expand the Arizona Cactus League in the late 1980s\n" +
-                    "when Florida was threatening to lure Cactus League teams to the Grapefruit League. Thanks to him,\n" +
-                    "fifteen MLB teams and their fans come to Arizona every spring to begin another exciting season."));
+                    "A mover and shaker in every sense of those words, Jim Bruner has certainly achieved legendary status in Arizona’s baseball history. Pursuing his interest in bringing a Major League Baseball franchise to Arizona, Bruner worked his influence as a member of both the Scottsdale City Council and the Maricopa County Board of Supervisors to persuade sports mogul Jerry Colangelo to form an ownership group. And, it was Bruner’s deciding vote that implemented the funding mechanism for a county-owned stadium, home of the Arizona Diamondbacks. Bruner served for three and a half years ashen-governor Rose Mofford to stabilize and expand the Arizona Cactus League in the late 1980s when Florida was threatening to lure Cactus League teams to the Grapefruit League. Thanks to him fifteen MLB teams and their fans come to Arizona every spring to begin another exciting season."));
         }
         if (year == 2017) {
             list.add(new com.example.mesabaseballexhibit.features.Inductee(R.drawable.dwight_patterson,
